9cce32b585823084e5dc660177fa9e58522aedf9
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-06-29 18:08:11 +0530

    WebAppDeploy

---
+++ RollBasedAuthentication/src/main/java/com/example/demo/User.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import java.util.*;
+
+import jakarta.persistence.*;
+ 
+@Entity
+@Table(name = "users1")
+public class User {
+ 
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    private String full_name;
+    private String password;
+    private boolean enabled;
+     
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+            )
+    private Set<Role> roles = new HashSet<>();
+ 
+    public Long getId() {
+        return id;
+    }
+	public String getFull_name() {
+		return full_name;
+	}
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
